f645ba5c86c6a6560da273974ec4e0bc0b999ab8
Chris Y <chyo1419@colorado.edu>
2023-08-01 02:36:41 +0000

    added basketball table

--- app.py
+++ app.py
@@ -11,4 +11,21 @@ def hello_world():
 def testing():
     conn = psycopg2.connect('postgres://lab_10_postgre_user:yEHgdwGoczjfLI3JdgzAt0YSGN0zHha8@dpg-cj46mhd9aq047cahvoc0-a/lab_10_postgre')
     conn.close()
-    return "Now ive fixed it?"
+    return "Database Connection Successful"
+
+@app.rout('/db_create')
+def creating():
+    conn = psycopg2.connect('postgres://lab_10_postgre_user:yEHgdwGoczjfLI3JdgzAt0YSGN0zHha8@dpg-cj46mhd9aq047cahvoc0-a/lab_10_postgre')
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+            First VARCHAR(255) , 
+            Last VARCHAR(255) , 
+            City VARCHAR(255) , 
+            Name VARCHAR(255) , 
+            Number INT
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
